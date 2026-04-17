6097092cf46012e12fe7217644cd4d6ea3d5b763
lawwee <lawalmohammed567@gmail.com>
2026-04-17 13:01:49 +0100

    created prisma service to connect to app

---
+++ src/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, Logger, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from './generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    private readonly logger = new Logger(PrismaService.name);
+
+    constructor() {
+        const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+        const adapter = new PrismaPg(pool);
+        super({ adapter });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+        this.logger.log('Database connected successfully');
+    };
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    };
+};
